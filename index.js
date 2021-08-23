b4950a108ece5965d64a02ae51e6b589183fcca4
Charles-P. Clermont <charles@cpclermont.com>
2021-08-23 15:44:52 -0400

    Simplify arguments & improve preview URL
    * Use the GitHub action run URL as a preview URL when no preview URL is
    found.
    * Flag build as failure when preview_url is missing.
    * Use GITHUB_* environment variables where we can to simplify arguments.

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function main() {
-  const ref = core.getInput('ref', { required: true });
-  const sha = core.getInput('sha', { required: true });
+  const ref = process.env.GITHUB_REF;
+  const sha = process.env.GITHUB_SHA;
+  const creator = process.env.ACTOR;
+  const token = core.getInput('token', { required: true });
   const environment = core.getInput('environment', {
     required: true,
   });
   const preview_url = core.getInput('preview_url');
   const description = core.getInput('description');
-  const creator = core.getInput('creator', {
-    required: true,
-  });
-  const token = core.getInput('token', { required: true });
   const error = core.getInput('error');
 
   const octokit = github.getOctokit(token, {
@@ -35,7 +33,7 @@ async function main() {
       environment,
       preview_url,
       description,
-    }
+    },
   };
 
   const resp = await octokit.rest.repos.createDeployment(req);
@@ -44,19 +42,20 @@ async function main() {
     throw new Error('Failed to create a new deployment');
   }
 
+  const isSuccessful = preview_url && preview_url.length > 0;
+
   const deploymentStatus = {
     repo,
     owner,
     deployment_id: resp.data.id,
-    environment_url: preview_url,
-    state: 'success',
   };
 
-  if(error) {
-    Object.assign(deploymentStatus, {
-      state: 'failure',
-      description: error
-    });
+  if (isSuccessful) {
+    deploymentStatus.state = 'success';
+    deploymentStatus.environment_url = preview_url;
+  } else {
+    deploymentStatus.state = 'failure';
+    deploymentStatus.environment_url = `https://github.com/${process.env.GITHUB_REPOSITORY}/runs/${process.env.GITHUB_RUN_ID}?check_suite_focus=true`;
   }
 
   await octokit.rest.repos.createDeploymentStatus(deploymentStatus);
